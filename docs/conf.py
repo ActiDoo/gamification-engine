e41a1ac89506e0996d0dc7750d4ecfd2496076e0
Marcel <jmsander@mail.upb.de>
2015-03-02 21:44:12 +0100

    trying to fix config for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return Mock()
 
-MOCK_MODULES = ['pymemcache.client','pylibmc']
+MOCK_MODULES = ['pymemcache.client','pylibmc','pyramid.config']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
