25159a47e900e3d8666e6046a639fc91b04faada
Matthias Feldotto <feldi@actidoo.com>
2015-03-05 20:58:16 +0100

    update version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ requires = [
     ]
 
 setup(name='gamification-engine',
-      version='0.1.2',
+      version='0.1.3',
       description='gamification-engine',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
