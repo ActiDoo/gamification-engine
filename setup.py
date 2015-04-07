2981dc9ec643c2d16ce356686fa02d377f08afbb
Marcel <jmsander@mail.upb.de>
2015-04-07 15:04:38 +0200

    update release

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ requires = [
     ]
 
 setup(name='gamification-engine',
-      version='0.1.14',
+      version='0.1.15',
       description='gamification-engine',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
