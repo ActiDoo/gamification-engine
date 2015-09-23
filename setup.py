479563924479f73e2dd54ea669f446c3909f2bd3
Marcel <jmsander@mail.upb.de>
2015-09-23 12:38:31 +0200

    update version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ requires = [
     ]
 
 setup(name='gamification-engine',
-      version='0.1.23',
+      version='0.1.24',
       description='The Gamification-Engine (gengine) provides an API for integrating any kinds of gamification features.',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
