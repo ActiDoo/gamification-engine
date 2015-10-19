8a44765c78fed1f30da34a9aa7d825a03012f929
feldi <feldi@actidoo.com>
2015-10-19 22:34:35 +0200

    update version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requires = [
     ]
 
 setup(name='gamification-engine',
-      version='0.1.32',
+      version='0.1.33',
       description='The Gamification-Engine (gengine) provides an API for integrating any kinds of gamification features.',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
