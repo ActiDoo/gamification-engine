cae48d9e9c3b12e35388e356fd655d52da0bb0c1
Marcel <jmsander@mail.upb.de>
2016-11-08 14:15:16 +0100

    travis

--- gengine/app/tests/db.py
+++ gengine/app/tests/db.py
@@ -1,11 +1,16 @@
 import testing.postgresql
+import os
 
 db = None
 
 def setupDB():
     # Generate Postgresql class which shares the generated database
     global db
-    db = testing.postgresql.PostgresqlFactory(cache_initialized_db=True)
+    db = testing.postgresql.PostgresqlFactory(
+        postgres=os.environ.get("TEST_POSTGRES",None),
+        initdb=os.environ.get("TEST_INITDB",None),
+        cache_initialized_db=True
+    )
 
 def unsetupDB():
     # clear cached database at end of tests
