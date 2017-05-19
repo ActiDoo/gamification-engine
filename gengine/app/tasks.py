03a283fadb3cd1c8e43332abf38dca72b9b7f20d
Sander <->
2017-05-19 13:14:34 +0200

    remove demo task

--- gengine/app/tasks.py
+++ gengine/app/tasks.py
@@ -3,19 +3,19 @@ from .tasksystem import EngineTask
 
 def includeme(config):
 
-    @EngineTask(
-        name="demo_task",
-        description="print sth",
-        config_scheme=None,
-        default_config=None,
-        default_cron="* * * * *",
-        default_activated=True
-    )
-    def demo_task(config):
-        return {
-            'log': None,
-            'success': True
-        }
+    # @EngineTask(
+    #     name="demo_task",
+    #     description="print sth",
+    #     config_scheme=None,
+    #     default_config=None,
+    #     default_cron="* * * * *",
+    #     default_activated=True
+    # )
+    # def demo_task(config):
+    #     return {
+    #         'log': None,
+    #         'success': True
+    #     }
 
 
     from gengine.app.registries import get_task_registration_points_registry
